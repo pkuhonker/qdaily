25d4297e308968d31f634601f10737bb870839aa
xzper <mail@xzper.com>
2017-06-21 18:02:56 +0800

    clean up

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,10 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import Navigation from './containers/Navigation';
-import SplashScreen from 'react-native-smart-splash-screen';
 
 const store = configureStore();
 
 export default class Astro extends React.Component<any, any> {
-
-    public componentDidMount(): void {
-        SplashScreen.close({
-            animationType: SplashScreen.animationType.fade,
-            duration: 500,
-            delay: 4000
-        });
-    }
 
     render() {
         return (

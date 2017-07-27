ebae9ee2728d696ae143980bb602ae665544e3d9
xzper <xzper@xzperdeMac-mini.local>
2017-07-27 11:46:10 +0800

    update sharesdk to 4.0.1

--- app/src/store/configureStore.ts
+++ app/src/store/configureStore.ts
@@ -17,7 +17,7 @@ const middlewares: Middleware[] = [
 ];
 
 export default function configureStore(initialState?: any): Store<any> {
-    const store = compose(
+    const store = compose<any>(
         applyMiddleware(...middlewares),
         autoRehydrate({ log: __DEV__ })
     )(createStore)(reducers, initialState);

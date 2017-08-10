076f0e321551b52b535e3545f2ba70dbbfdb0a59
xzper <xzper@xzperdeMac-mini.local>
2017-08-10 20:46:53 +0800

    rename umeng native module

---
+++ app/ios/UmengNativeModule.h
@@ -0,0 +1,13 @@
+//
+//  UmengNativeModule.h
+//  UmengNativeModule
+//
+//
+//  Copyright (c) 2016年 tendcloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+@interface UmengNativeModule : NSObject  <RCTBridgeModule>
+
+@end
